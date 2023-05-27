feae19b6a4e4037a1b8bb5d8a8b23e11a71e5dab
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-05-27 18:46:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'primereact/button';
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign:'center'}}>TO-DO-LIST</h1>
+      <Button label="Submit" icon="pi pi-check" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import "primereact/resources/primereact.min.css";    
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import 'primeicons/primeicons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
