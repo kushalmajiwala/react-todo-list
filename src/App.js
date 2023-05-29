332218378bf3686fd7d54f0270d12d958eb0ea96
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-05-29 19:38:19 +0530

    todo list completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,83 @@
 import { InputText } from 'primereact/inputtext';
 import { Tooltip } from 'primereact/tooltip';
+import { useState, useEffect } from 'react';
 import './style.css';
 
+//Getting Local Storage Data
+const getLocalData = () => {
+  const lists = localStorage.getItem("mytodolist");
+  if(lists)
+  {
+    return JSON.parse(lists);
+  }
+  else return [];
+}
+
 function App() {
+  const [inputdata, setInputdata] = useState("");
+  const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
+
+  // Add Item Function
+  const addItem = () => {
+    if (!inputdata) {
+      alert("Please Enter Data");
+    }
+    else {
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: inputdata
+      }
+      setItems([...items, myNewInputData]);
+      setInputdata("");
+    }
+  }
+
+  // Delete Item Function
+  const deleteItem = (delid) => {
+    const updatedItems = items.filter((currentElem) => {
+      return delid !== currentElem.id;
+    });
+    setItems(updatedItems);
+  }
+
+  //Edit Item Function
+  const editItem = (editid) => {
+    const updatingItem = items.find ((currentElem) => {
+      return editid === currentElem.id;
+    });
+    setInputdata(updatingItem.name);
+    setIsEditItem(editid);
+    setToggleButton(true);
+  }
+
+  // Performing Edit Item Function
+  const performEdit = () => {
+    const updatingItem = items.map((currentElem) => {
+      if(currentElem.id === isEditItem)
+      {
+        return { ...currentElem, name: inputdata};
+      }
+      return currentElem;
+    });
+    updatingItem.name = inputdata;
+    setItems(updatingItem);
+    setIsEditItem("");
+    setInputdata("");
+    setToggleButton(false);
+  }
+
+  // Check All Items
+  const checkAllItems = () => {
+    setItems([]);
+  }
+
+  //Adding in Local Storage
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(items));
+  }, [items]);
+
   return (
     <>
       <div className='img-heading'>
@@ -13,29 +88,23 @@ function App() {
         <div>
           <span className="p-input-icon-left p-input-icon-right inputField">
             <i class="bi bi-pencil-fill penIcon"></i>
-            <InputText placeholder="Add Item" style={{ width: '100%' }} />
-            <i class="pi pi-plus plusIcon addItem" data-pr-tooltip='Add Item' data-pr-position="right"></i>
-            <Tooltip target=".addItem" style={{ marginLeft: '1%' }} />
+            <InputText placeholder="Add Item" style={{ width: '100%' }} value={inputdata} onChange={(e) => setInputdata(e.target.value)} />
+            {toggleButton ? ( <i class="bi bi-pencil-square performEditIcon" onClick={performEdit}></i> ) : ( <i class="pi pi-plus plusIcon addItem" data-pr-tooltip='Add Item' data-pr-position="right" onClick={addItem}></i> )}
+            <Tooltip target=".addItem" className='tooltipVis' style={{ marginLeft: '1%' }} />
           </span>
         </div>
         <div className='listContent'>
-          <div className='listItem'>
-            <div className='listName'>mango</div>
-            <div className='listIcons'>
-              <i class="bi bi-pencil-square editIcon"></i>
-              <i class="bi bi-trash3-fill deleteIcon"></i>
+          {items.map((currentItem) => (
+            <div className='listItem' key={currentItem.id}>
+              <div className='listName'>{ currentItem.name }</div>
+              <div className='listIcons'>
+                <i class="bi bi-pencil-square editIcon" onClick={() => editItem(currentItem.id)}></i>
+                <i class="bi bi-trash3-fill deleteIcon" onClick={() => deleteItem(currentItem.id)}></i>
+              </div>
             </div>
-          </div>
-          <div className='listItem'>
-            <div className='listName'>banana</div>
-            <div className='listIcons'>
-              <i class="bi bi-pencil-square editIcon"></i>
-              <i class="bi bi-trash3-fill deleteIcon"></i>
-            </div>
-          </div>
-         
+          ))}
         </div>
-        <button className="checkbtn">CHECK LIST</button>
+        <button className="checkbtn" onClick={checkAllItems}>CHECK LIST</button>
       </div>
     </>
   );
