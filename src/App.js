2b683c0f043eb853cc80338f7debea570cf19926
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-05-30 19:24:13 +0530

    complete project with database

--- src/App.js
+++ src/App.js
@@ -1,82 +1,77 @@
 import { InputText } from 'primereact/inputtext';
 import { Tooltip } from 'primereact/tooltip';
 import { useState, useEffect } from 'react';
+import { createClient } from "@supabase/supabase-js";
 import './style.css';
 
-//Getting Local Storage Data
-const getLocalData = () => {
-  const lists = localStorage.getItem("mytodolist");
-  if(lists)
-  {
-    return JSON.parse(lists);
-  }
-  else return [];
-}
+const supabase = createClient("https://ivabqohtumjadxnipnsa.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2YWJxb2h0dW1qYWR4bmlwbnNhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NTQyMTI0MywiZXhwIjoyMDAwOTk3MjQzfQ.X41oJJSXkbe1kRkFm_1UyufrUN_wHwHVHe_xz3HF72A");
 
 function App() {
   const [inputdata, setInputdata] = useState("");
-  const [items, setItems] = useState(getLocalData());
+  const [items, setItems] = useState([]);
   const [isEditItem, setIsEditItem] = useState("");
   const [toggleButton, setToggleButton] = useState(false);
 
   // Add Item Function
-  const addItem = () => {
+  const addItem = async () => {
     if (!inputdata) {
       alert("Please Enter Data");
     }
     else {
-      const myNewInputData = {
-        id: new Date().getTime().toString(),
-        name: inputdata
+      const insertData = {
+        id: new Date().getTime(),
+        item_name: inputdata
       }
-      setItems([...items, myNewInputData]);
+      const { error } = await supabase.from("list_items").insert(insertData);
+      if(error) throw error;
       setInputdata("");
+      getListData();
     }
   }
 
   // Delete Item Function
-  const deleteItem = (delid) => {
-    const updatedItems = items.filter((currentElem) => {
-      return delid !== currentElem.id;
-    });
-    setItems(updatedItems);
+  const deleteItem = async (delid) => {
+    const { error } = await supabase.from("list_items").delete().eq('id', delid);
+    if(error) throw error;
+    getListData();
   }
 
   //Edit Item Function
   const editItem = (editid) => {
-    const updatingItem = items.find ((currentElem) => {
+    const updatingItem = items.find((currentElem) => {
       return editid === currentElem.id;
     });
-    setInputdata(updatingItem.name);
+    setInputdata(updatingItem.item_name);
     setIsEditItem(editid);
     setToggleButton(true);
   }
 
   // Performing Edit Item Function
-  const performEdit = () => {
-    const updatingItem = items.map((currentElem) => {
-      if(currentElem.id === isEditItem)
-      {
-        return { ...currentElem, name: inputdata};
-      }
-      return currentElem;
-    });
-    updatingItem.name = inputdata;
-    setItems(updatingItem);
+  const performEdit = async () => {
+    const { error } = await supabase.from("list_items").update({ item_name: inputdata }).eq('id', isEditItem);
+    if(error) throw error;
+    getListData();
     setIsEditItem("");
     setInputdata("");
     setToggleButton(false);
   }
 
   // Check All Items
-  const checkAllItems = () => {
-    setItems([]);
+  const checkAllItems = async () => {
+    const { error } = await supabase.from("list_items").delete().neq("id", 0);
+    if(error) throw error;
+    getListData();
   }
 
   //Adding in Local Storage
   useEffect(() => {
-    localStorage.setItem("mytodolist", JSON.stringify(items));
-  }, [items]);
+    getListData();
+  }, []);
+
+  const getListData = async () => {
+    const { data } = await supabase.from("list_items").select().order('id', { ascending: false });
+    setItems(data);
+  }
 
   return (
     <>
@@ -89,14 +84,14 @@ function App() {
           <span className="p-input-icon-left p-input-icon-right inputField">
             <i class="bi bi-pencil-fill penIcon"></i>
             <InputText placeholder="Add Item" style={{ width: '100%' }} value={inputdata} onChange={(e) => setInputdata(e.target.value)} />
-            {toggleButton ? ( <i class="bi bi-pencil-square performEditIcon" onClick={performEdit}></i> ) : ( <i class="pi pi-plus plusIcon addItem" data-pr-tooltip='Add Item' data-pr-position="right" onClick={addItem}></i> )}
+            {toggleButton ? (<i class="bi bi-pencil-square performEditIcon" onClick={performEdit}></i>) : (<i class="pi pi-plus plusIcon addItem" data-pr-tooltip='Add Item' data-pr-position="right" onClick={addItem}></i>)}
             <Tooltip target=".addItem" className='tooltipVis' style={{ marginLeft: '1%' }} />
           </span>
         </div>
         <div className='listContent'>
           {items.map((currentItem) => (
             <div className='listItem' key={currentItem.id}>
-              <div className='listName'>{ currentItem.name }</div>
+              <div className='listName'>{currentItem.item_name}</div>
               <div className='listIcons'>
                 <i class="bi bi-pencil-square editIcon" onClick={() => editItem(currentItem.id)}></i>
                 <i class="bi bi-trash3-fill deleteIcon" onClick={() => deleteItem(currentItem.id)}></i>
